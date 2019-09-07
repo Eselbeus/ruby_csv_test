f497086043abdb09a73918438ef365ea433bc048
github username <github email address>
2019-09-07 00:11:17 -0400

    it works but going to double check this is what it is supposed to be

--- bin/csv_parser.rb
+++ bin/csv_parser.rb
@@ -12,6 +12,7 @@ def yesterdays_date
   year = date.to_s.slice(2, 2)
   month = date.to_s.slice(5, 2)
   day = date.to_s.slice(8, 2)
+
   year + month + day
 end
 
@@ -24,23 +25,39 @@ def converter (file)
     if i == 0
       final_text.push("92" + yesterdays_date)
     else
-      sales_count += 1
-      zipcode = line[1]
-      if zipcode == nil
-        zipcode = ""
+      transaction_counter = 1
+
+      units_per_transaction = line[6]
+      if units_per_transaction.to_i > 5
+        units_per_transaction = 5
       end
-      fixed_data = "D3|12345678|"
-      fixed_data2 = "|S|"
-      fixed_data3 = "||1000|A|P"
-      final_text.push(fixed_data + zipcode + fixed_data2 + fixed_data3)
+
+      while transaction_counter <= units_per_transaction.to_i
+        sales_count += 1
+        fixed_data = "D3|12345678|"
+        zipcode = line[1]
+        fixed_data2 = "|S|00"
+        fixed_data3 = "||1000|A|P"
+
+        if zipcode == nil
+          zipcode = ""
+        end
+
+        final_text.push(fixed_data + zipcode + fixed_data2 + transaction_counter.to_s + fixed_data3)
+
+        transaction_counter += 1
+      end
+
     end
   end
+
   sales_count = sales_count.to_s
   final_text.push("94|" + sales_count + "|" + sales_count)
+
   final_text = final_text.join("\n")
   File.open("test_file.txt", "a" ) {|f| f.write(final_text)}
 
-  final_text
+  return final_text
 end
 
 puts converter(file)
