760dd642ac9120412ce5945c183a498018da04d7
github username <github email address>
2019-09-07 01:19:22 -0400

    refactoring

--- bin/csv_parser.rb
+++ bin/csv_parser.rb
@@ -5,7 +5,7 @@ require 'date'
 
 file = CSV.read("./Seann_Sheet1.csv")
 
-File.open("test_file.txt", "w")
+File.open("converted_file.txt", "w")
 
 def yesterdays_date
   date = Date.today.prev_day
@@ -16,48 +16,46 @@ def yesterdays_date
   year + month + day
 end
 
-def converter (file)
-  sales_count = 0
+def data_line (zipcode, sale_counter)
+  if zipcode == nil
+    zipcode = ""
+  end
 
+  "D3|12345678|#{zipcode}|S|00#{sale_counter.to_s}||1000|A|P"
+end
+
+def converter (file)
   final_text = []
+  total_sales = 0 #total sales gets incremented inside while loop so each of the sales within a transaction count
 
   file.each_with_index do |line, i|
     if i == 0
-      final_text.push("92" + yesterdays_date)
+      final_text.push("92" + yesterdays_date) #builds first line
     else
-      transaction_counter = 1
+      sales_counter = 1 #initializes loop that gives each sale a number 001-005
+      sales_per_transaction = line[6] #this is the number of sales per transaction from the csv
 
-      units_per_transaction = line[6]
-      if units_per_transaction.to_i > 5
-        units_per_transaction = 5
+      if sales_per_transaction.to_i > 5
+        sales_per_transaction = 5
       end
 
-      while transaction_counter <= units_per_transaction.to_i
-        sales_count += 1
-        fixed_data = "D3|12345678|"
+      while sales_counter <= sales_per_transaction.to_i
+        total_sales += 1
         zipcode = line[1]
-        fixed_data2 = "|S|00"
-        fixed_data3 = "||1000|A|P"
+        final_text.push(data_line(zipcode, sales_counter))
 
-        if zipcode == nil
-          zipcode = ""
-        end
-
-        final_text.push(fixed_data + zipcode + fixed_data2 + transaction_counter.to_s + fixed_data3)
-
-        transaction_counter += 1
+        sales_counter += 1
       end
-
     end
   end
 
-  sales_count = sales_count.to_s
-  final_text.push("94|" + sales_count + "|" + sales_count)
+  total_sales = total_sales.to_s
+  final_text.push("94|" + total_sales + "|" + total_sales)
 
-  final_text = final_text.join("\n")
-  File.open("test_file.txt", "a" ) {|f| f.write(final_text)}
+  final_text = final_text.join("\n") #converts from array into string format with each element on a new line
+  File.open("converted_file.txt", "a" ) {|f| f.write(final_text)} #writes the entire completed string to a text file
 
   return final_text
 end
 
-puts converter(file)
+converter(file)
