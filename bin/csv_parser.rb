36ac3b572988422ded1a0cdeb3a97410ec2df891
github username <github email address>
2019-09-06 22:14:27 -0400

    text file writes

--- bin/csv_parser.rb
+++ bin/csv_parser.rb
@@ -3,34 +3,26 @@
 require 'csv'
 require 'date'
 
-puts "hello world"
-
 file = CSV.read("./Seann_Sheet1.csv")
 
+File.open("test_file.txt", "w")
 
-
-# print file
-
-# date = Date.today.prev_day
-# year = date.to_s.slice(2, 2)
-# month = date.to_s.slice(5, 2)
-# day = date.to_s.slice(8, 2)
-# yesterday = year + month + day
-# line_one = "92" + yesterday
-
-def converter (file)
-  sales_count = 0
-
+def yesterdays_date
   date = Date.today.prev_day
   year = date.to_s.slice(2, 2)
   month = date.to_s.slice(5, 2)
   day = date.to_s.slice(8, 2)
-  sales = sales_count
+  year + month + day
+end
+
+def converter (file)
+  sales_count = 0
+
   final_text = []
 
   file.each_with_index do |line, i|
     if i == 0
-      final_text.push("92" + year + month + day)
+      final_text.push("92" + yesterdays_date)
     else
       sales_count += 1
       zipcode = line[1]
@@ -39,11 +31,14 @@ def converter (file)
       end
       fixed_data = "D3|12345678|"
       fixed_data2 = "|S|"
-      final_text.push(fixed_data + zipcode + fixed_data2)
+      fixed_data3 = "||1000|A|P"
+      final_text.push(fixed_data + zipcode + fixed_data2 + fixed_data3)
     end
   end
-  final_text.push(sales_count - 1)
-
+  sales_count -= 1
+  sales_count = sales_count.to_s
+  final_text.push("94|" + sales_count + "|" + sales_count)
+  File.open("test_file.txt", "a" ) {|f| f.write(final_text)}
   # final_text += lines
 
   return final_text
